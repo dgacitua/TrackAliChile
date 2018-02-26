6f0e3c067acbd51decf9684614673c39991e4f91
Daniel Gacitua <daniel.gacitua@usach.cl>
2018-02-25 22:20:51 -0300

    Add PM2 deploy support

---
+++ ecosystem.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+    {
+      name      : 'TrackAliChile',
+      script    : './dist/server/index.js'
+    }
+  ],
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : 'dgacitua',
+      host : [ 'dgacitua.info' ],
+      ref  : 'origin/master',
+      repo : 'git@github.com:dgacitua/TrackAliChile.git',
+      path : '/var/www/production',
+      'post-deploy' : 'npm install && npm run build && npm run deploy-client && pm2 reload ecosystem.config.js --env production',
+      env  : {
+        NODE_ENV: 'production'
+      }
+    },
+    dev : {
+      user : 'dgacitua',
+      host : [ 'localhost' ],
+      ref  : 'origin/master',
+      repo : 'git@github.com:dgacitua/TrackAliChile.git',
+      path : '/var/www/development',
+      'post-deploy' : 'npm install && npm run build && npm run deploy-client && pm2 reload ecosystem.config.js --env production',
+      env  : {
+        NODE_ENV: 'dev'
+      }
+    }
+  }
+};
